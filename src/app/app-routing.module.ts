09698f09d73f8a59c69276a51d803bf7f72234f2
WillemPolfliet <willem.polfliet@cegeka.com>
2018-11-28 10:10:24 +0100

    comming test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent }  from './home-page/home-page.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomePageComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
